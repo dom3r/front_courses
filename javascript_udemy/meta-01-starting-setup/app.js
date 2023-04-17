3ad0dd77ff00fa02cf812519ba5ffe516a90d80b
Dom3r <kubsler123@gmail.com>
2023-04-17 16:07:59 +0200

    Meta-programming

--- javascript_udemy/meta-01-starting-setup/app.js
+++ javascript_udemy/meta-01-starting-setup/app.js
@@ -6,11 +6,11 @@ const person = {
   [uid]: "p1",
   age: 30,
   name: "James",
-  [Symbol.toStringTag]: "User Object",
+  [Symbol.toStringTag]: "User Objectss",
 };
 
 person.uid = "p2";
-
+// Symbol
 console.log(person);
 console.log(Symbol("uid") === Symbol("uid"));
 console.log(person.toString());
@@ -29,6 +29,7 @@ const company = {
     this.curEmployee++;
     return returnValue;
   },
+  // Iterator
   [Symbol.iterator]: function* employeeGenerator() {
     let curEmployee = 0;
     while (curEmployee < this.employees.length) {
@@ -57,3 +58,50 @@ console.log(persons);
 for (const person of persons) {
   console.log(person);
 }
+
+// reflect api
+
+const course = {
+  title: "JavaScript - The Complete Guide",
+  rating: 5,
+};
+
+Reflect.setPrototypeOf(course, {
+  toString() {
+    return this.title;
+  },
+});
+Reflect.defineProperty(course, "price", {
+  value: 100,
+  writable: true,
+  enumerable: true,
+  configurable: true,
+});
+// Object vs Reflect API - Reflect is newer, also have delete property method
+Reflect.deleteProperty(course, "rating");
+console.log(course.toString());
+console.log(course);
+
+// proxy api
+
+const courseHandler = {
+  get(obj, propertyName) {
+    if (propertyName === "length") {
+      return 0;
+    }
+    return obj[propertyName] || "NOT FOUND";
+  },
+  set(obj, propertyName, newValue) {
+    if (propertyName === "rating") {
+      return;
+    }
+    obj[propertyName] = newValue;
+  },
+};
+
+const pCourse = new Proxy(course, courseHandler);
+pCourse.numbero = 5;
+console.log(pCourse.pog);
+console.log(pCourse.title);
+console.log(pCourse.numbero);
+console.log(pCourse);
